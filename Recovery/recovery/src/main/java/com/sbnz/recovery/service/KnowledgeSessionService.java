6fd54b3df816124cbe93f6c0d4d32f8fa6e6815f
evaj10 <evaj.evadesign@gmail.com>
2021-05-18 13:10:10 +0200

    Moved session creation to service class

---
+++ Recovery/recovery/src/main/java/com/sbnz/recovery/service/KnowledgeSessionService.java
@@ -0,0 +1,44 @@
+package com.sbnz.recovery.service;
+
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KnowledgeSessionService {
+
+	private static Logger log = LoggerFactory.getLogger(KnowledgeSessionService.class);
+	
+	private final KieContainer kieContainer;
+    private KieSession rulesSession;
+
+    @Autowired
+    public KnowledgeSessionService(KieContainer kieContainer) {
+        this.kieContainer = kieContainer;
+    }
+
+    public KieContainer getKieContainer() {
+        return kieContainer;
+    }
+
+    public KieSession getRulesSession() {
+    	if (this.rulesSession == null) {
+    		log.info("Initialized session");
+        	rulesSession = kieContainer.newKieSession("rulesSession");	
+    	}
+        return rulesSession;
+    }
+
+    public void setRulesSession(KieSession kieSession) {
+        this.rulesSession = kieSession;
+    }
+    
+    public void releaseRulesSession(){
+        this.rulesSession.dispose();
+        this.rulesSession = null;
+    }
+    
+}
--- Recovery/recovery/src/main/java/com/sbnz/recovery/service/TestService.java
+++ Recovery/recovery/src/main/java/com/sbnz/recovery/service/TestService.java
@@ -1,9 +1,6 @@
 package com.sbnz.recovery.service;
 
-import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +9,13 @@ import com.sbnz.recovery.model.Patient;
 @Service
 public class TestService {
 
-	private static Logger log = LoggerFactory.getLogger(TestService.class);
-
-	private final KieContainer kieContainer;
-
 	@Autowired
-	public TestService(KieContainer kieContainer) {
-		log.info("Initialising a new example session.");
-		this.kieContainer = kieContainer;
-	}
-
+	private KnowledgeSessionService knowledgeSessionService;
+	
 	public Patient calculateBmi(Patient patient) {
-		KieSession kieSession = kieContainer.newKieSession();
+		KieSession kieSession = knowledgeSessionService.getRulesSession();
 		kieSession.insert(patient);
 		kieSession.fireAllRules();
-		kieSession.dispose();
 		return patient;
 	}
 }
